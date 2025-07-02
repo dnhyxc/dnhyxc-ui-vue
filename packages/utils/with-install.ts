d7c77d315b288550b324d5ea47bd1ba27244ceb8
dnhyxc <2640559435@qq.com>
2025-07-02 09:23:38 +0800

    feat: utils

--- packages/utils/with-install.ts
+++ packages/utils/with-install.ts
@@ -1,11 +1,12 @@
-import { Plugin } from 'vue';
+import { type Plugin, type App } from 'vue';
 
 export type SFCWithInstall<T> = T & Plugin;
 
 // 为组件增加install方法，使组件及可以通过 use 方式注册为全局组件，也可以通过 import 方式单独使用
 export const withInstall = <T>(comp: T) => {
-  (comp as SFCWithInstall<T>).install = (app: { component: (arg0: string, arg1: T) => void }) => {
-    app.component((comp as unknown as { name: string }).name, comp);
+  const c = comp as unknown as { name: string };
+  (comp as SFCWithInstall<T>).install = (app: App) => {
+    app.component(c.name, c);
   };
   return comp as SFCWithInstall<T>;
 };
