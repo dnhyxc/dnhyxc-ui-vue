09944fdbdf6e88c034a7544dbba6d220a0c2d2f8
dnhyxc <2640559435@qq.com>
2025-07-02 09:17:07 +0800

    feat: eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,97 @@
+import eslint from '@eslint/js';
+import tsEslintPlugin from '@typescript-eslint/eslint-plugin';
+import tsEslintParser from '@typescript-eslint/parser';
+import vuePlugin from 'eslint-plugin-vue';
+import vueParser from 'vue-eslint-parser';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+
+const customTsFlatConfig = [
+  {
+    name: 'typescript-eslint/base',
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsEslintParser,
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: { jsx: false }
+      }
+    },
+    rules: {
+      ...tsEslintPlugin.configs.recommended.rules,
+      '@typescript-eslint/no-confusing-non-null-assertion': 2,
+    },
+    plugins: {
+      '@typescript-eslint': tsEslintPlugin,
+    },
+  },
+];
+
+const flatConfig = [
+  // 全局配置
+  {
+    name: 'global config',
+    languageOptions: {
+      globals: {
+        ...globals.es2022,
+        ...globals.browser,
+        ...globals.node,
+      },
+      sourceType: 'module', // 确保设置为 module
+    },
+    rules: {
+      'no-dupe-class-members': 0,
+      'no-redeclare': 0,
+      'no-undef': 0,
+      'no-unused-vars': 0,
+    },
+  },
+
+  // Vue 文件配置
+  {
+    name: 'vue-eslint',
+    files: ['**/*.vue'],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: tsEslintParser, // Vue 文件中使用 TS 解析器
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        extraFileExtensions: ['.vue'],
+        ecmaFeatures: { jsx: false }
+      }
+    },
+    plugins: {
+      vue: vuePlugin,
+    },
+    rules: {
+      ...vuePlugin.configs['vue3-recommended'].rules,
+      'vue/multi-word-component-names': 'off',
+    }
+  },
+
+  // JS/TS 文件配置
+  {
+    files: ['**/*.{js,ts}'],
+    languageOptions: {
+      parser: tsEslintParser, // 统一使用 TS 解析器
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: { jsx: false }
+      }
+    }
+  },
+
+  // 忽略文件
+  {
+    ignores: ['dist/**', 'node_modules/**'],
+  }
+];
+
+export default [
+  eslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+  ...flatConfig,
+  ...customTsFlatConfig,
+];
--- packages/utils/with-install.ts
+++ packages/utils/with-install.ts
@@ -4,7 +4,7 @@ export type SFCWithInstall<T> = T & Plugin;
 
 // 为组件增加install方法，使组件及可以通过 use 方式注册为全局组件，也可以通过 import 方式单独使用
 export const withInstall = <T>(comp: T) => {
-  (comp as SFCWithInstall<T>).install = (app: any) => {
+  (comp as SFCWithInstall<T>).install = (app: { component: (arg0: string, arg1: T) => void }) => {
     app.component((comp as unknown as { name: string }).name, comp);
   };
   return comp as SFCWithInstall<T>;
